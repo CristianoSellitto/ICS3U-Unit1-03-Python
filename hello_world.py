c7c13785268a4aa63f3c0b9973e773d817a07b78
Cristiano Sellitto <cristiano.sellitto@mths.ca>
2022-09-15 14:54:10 +0000

    Added program

---
+++ hello_world.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# Created by Cristiano Sellitto
+# Created in September 2022
+# A basic Hello, World! program with proper style
+
+
+def main():
+    # Prints Hello, World! to the console
+
+    print("Hello, World!")
+
+    print("\nDone.")
+
+
+if __name__ == "__main__":
+    main()
